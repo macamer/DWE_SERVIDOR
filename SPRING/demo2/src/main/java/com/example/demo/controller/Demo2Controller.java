25a3f410223b1dfc9c6cfc3c0f324c95f65c9569
Maca <mariacavallerm@gmail.com>
2024-09-25 19:17:29 +0200

    Model ModelMap ModelAndView

--- SPRING/demo2/src/main/java/com/example/demo/controller/Demo2Controller.java
+++ SPRING/demo2/src/main/java/com/example/demo/controller/Demo2Controller.java
@@ -1,5 +1,58 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class Demo2Controller {
+
+	@Value("${aplicacion.nombre}")
+	private String nombreAplicacion;
+
+	// Definimos el objeto de log
+	private static final Logger loggerController = LoggerFactory.getLogger(Demo2Controller.class);
+
+	@GetMapping(value = "/helloModel")
+	public String hello(Model model) {
+		model.addAttribute("título", nombreAplicacion);
+		model.addAttribute("mensaje", "hola desde Thymeleaf con Model");
+
+		loggerController.info("Mostramos el mensaje de hola con Model");
+
+		// Retornamos la vista que mostrará los valores de los atributos
+		return "helloView";
+	}
+
+	@GetMapping(value = "/helloModelMap")
+	public String hello(ModelMap map) {
+		map.addAttribute("titulo", "ModelMap");
+		map.addAttribute("mensaje", "hola desde Thymeleaf con ModelMap");
+
+		loggerController.info("Mostramos el mensaje de hola con ModelMap");
+
+		// Retornamos la vista que mostrará los valores de los atributos
+		return "helloView";
+	}
+	
+	@GetMapping(value = "/helloModelAndView")
+	//el metodo se puede llamar igual que los anteriores
+	public ModelAndView helloMAV() {
+		ModelAndView mav  = new ModelAndView();
+		mav.addObject("titulo", nombreAplicacion);
+		mav.addObject("mensaje", "hola desde Thymeleaf con ModelAndView");
+
+		loggerController.info("Mostramos el mensaje de hola con ModelAndView");
+
+		mav.setViewName("helloView");
+		
+		// Retornamos la vista que mostrará los valores de los atributos
+		return mav;
+	}
 
 }
