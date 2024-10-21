393115cd07ce2631ae088287afd924bf040ce710
Maca <mariacavallerm@gmail.com>
2024-10-21 16:25:35 +0200

    configure index view on indexController

--- SPRING/demo5-lombok/src/main/java/com/example/demo/web/controller/IndexController.java
+++ SPRING/demo5-lombok/src/main/java/com/example/demo/web/controller/IndexController.java
@@ -1,15 +1,29 @@
 package com.example.demo.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+	
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+	
+	@Value("${aplicacion.nombre}") //link with value of application properties
+	private String nombreAplicacion;
+	
+	@Value("${asignatura.nombre}")
+	private String nombreAsignatura;
 
 	public ModelAndView index() {
+		
+		log.info("IndexController - index: Mostramos la página inicial");
+		
 		ModelAndView mav = new ModelAndView("index");
-		mav.addObject("titulo", mav);
-		mav.addObject("nombreAsignatura", mav);
+		mav.addObject("titulo", nombreAplicacion);
+		mav.addObject("nombreAsignatura", nombreAsignatura);
 		
 		return mav;
 	}
