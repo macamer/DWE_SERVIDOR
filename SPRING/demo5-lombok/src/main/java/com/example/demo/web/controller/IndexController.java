6c9f34b9ee3477d5012e130ab547615a2a162e39
Maca <mariacavallerm@gmail.com>
2024-10-22 08:21:38 +0200

    fix propierty issue

--- SPRING/demo5-lombok/src/main/java/com/example/demo/web/controller/IndexController.java
+++ SPRING/demo5-lombok/src/main/java/com/example/demo/web/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,9 +15,10 @@ public class IndexController {
 	@Value("${aplicacion.nombre}") //link with value of application properties
 	private String nombreAplicacion;
 	
-	@Value("${asignatura.nombre}")
+	@Value("${asignatura}")
 	private String nombreAsignatura;
 
+	@GetMapping("/")
 	public ModelAndView index() {
 		
 		log.info("IndexController - index: Mostramos la página inicial");
