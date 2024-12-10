b71405b99ef5f2553aaf6a94acebe5003fed0b88
Maca <mariacavallerm@gmail.com>
2024-12-10 19:19:03 +0100

    add equals & hashCode

--- SPRING/demo6/src/main/java/com/example/demo/repository/entity/Recomendacion.java
+++ SPRING/demo6/src/main/java/com/example/demo/repository/entity/Recomendacion.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository.entity;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -13,16 +15,33 @@ import lombok.ToString;
 
 @Data
 @Entity
-@Table(name="recomendaciones")
+@Table(name = "recomendaciones")
 public class Recomendacion {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String observaciones;
-	
-	@OneToOne(fetch=FetchType.LAZY, optional=false)
-	@JoinColumn(name="idcliente")
-	@ToString.Exclude //don't show Cliente into toString()
+
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "idcliente")
+	@ToString.Exclude // don't show Cliente into toString()
 	private Cliente cliente;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recomendacion other = (Recomendacion) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
