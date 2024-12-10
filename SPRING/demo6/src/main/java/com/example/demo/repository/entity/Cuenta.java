a18f030cd2d7b1a79f3bc6be7111c28a7e3ea5ac
Maca <mariacavallerm@gmail.com>
2024-12-10 19:33:08 +0100

    add ManyToOne & JoinColumn on Cliente

--- SPRING/demo6/src/main/java/com/example/demo/repository/entity/Cuenta.java
+++ SPRING/demo6/src/main/java/com/example/demo/repository/entity/Cuenta.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -28,6 +30,10 @@ public class Cuenta {
 	
 	@Column(name="saldoactual")
 	private float saldoActual;
+	
+	@ManyToOne
+	@JoinColumn(name="idcliente")
+	private Cliente cliente;
 
 	@Override
 	public boolean equals(Object obj) {
