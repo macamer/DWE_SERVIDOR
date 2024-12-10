11592861a7b65f934086b78f999dba943b969d8f
Maca <mariacavallerm@gmail.com>
2024-12-10 19:33:53 +0100

    add listaCuentasDTO atribute

--- SPRING/demo6/src/main/java/com/example/demo/model/dto/ClienteDTO.java
+++ SPRING/demo6/src/main/java/com/example/demo/model/dto/ClienteDTO.java
@@ -1,16 +1,21 @@
 package com.example.demo.model.dto;
 
 import java.io.Serializable;
-import lombok.Data;
-import com.example.demo.repository.entity.Cliente;
-import com.example.demo.repository.entity.Recomendacion;
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.Data;
+import lombok.ToString;
+
+import com.example.demo.repository.entity.Cliente;
+import com.example.demo.repository.entity.Cuenta;
+import com.example.demo.repository.entity.Recomendacion;
 
 //serialize the class, so it can be sent to web
 @Data
 public class ClienteDTO implements Serializable {
-	
-	//create a id for the class
+
+	// create a id for the class
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String nif;
@@ -18,11 +23,14 @@ public class ClienteDTO implements Serializable {
 	private String apellidos;
 	private String claveSeguridad;
 	private String email;
-	private RecomendacionDTO recomendacionDTO; //if we have an entity we have to transform to DTO class too
-	
-	//convert a entity to a DTO
+	private RecomendacionDTO recomendacionDTO; // if we have an entity we have to transform to DTO class too
+
+	@ToString.Exclude
+	private List<CuentaDTO> listaCuentasDTO;
+
+	// convert a entity to a DTO
 	public static ClienteDTO convertToDTO(Cliente cliente) {
-		//create new Cliente and assign basic data
+		// create new Cliente and assign basic data
 		ClienteDTO clienteDTO = new ClienteDTO();
 		clienteDTO.setId(cliente.getId());
 		clienteDTO.setNif(cliente.getNif());
@@ -30,15 +38,21 @@ public class ClienteDTO implements Serializable {
 		clienteDTO.setApellidos(cliente.getApellidos());
 		clienteDTO.setClaveSeguridad(cliente.getClaveSeguridad());
 		clienteDTO.setEmail(cliente.getEmail());
-		
-		RecomendacionDTO rec = RecomendacionDTO.convertToDTO(cliente.getRecomendacion(), clienteDTO); //convert to DTO
+
+		RecomendacionDTO rec = RecomendacionDTO.convertToDTO(cliente.getRecomendacion(), clienteDTO); // convert to DTO
 		clienteDTO.setRecomendacionDTO(rec);
-		
+
+		// Cargamos la lista de cuentas
+		for (int i = 0; i < cliente.getListaCuentas().size(); i++) {
+			CuentaDTO cuentadto = CuentaDTO.convertToDTO(cliente.getListaCuentas().get(i), clienteDTO);
+			clienteDTO.getListaCuentasDTO().add(cuentadto);
+		}
+
 		return clienteDTO;
 	}
-	
-	//convert DTO to a entity
-	public static Cliente contertToEntity(ClienteDTO clienteDTO) {
+
+	// convert DTO to a entity
+	public static Cliente convertToEntity(ClienteDTO clienteDTO) {
 		Cliente cliente = new Cliente();
 		cliente.setId(clienteDTO.getId());
 		cliente.setNif(clienteDTO.getNif());
@@ -46,18 +60,24 @@ public class ClienteDTO implements Serializable {
 		cliente.setApellidos(clienteDTO.getApellidos());
 		cliente.setClaveSeguridad(clienteDTO.getClaveSeguridad());
 		cliente.setEmail(clienteDTO.getEmail());
-		
+
 		Recomendacion rec = RecomendacionDTO.convertToEntity(clienteDTO.getRecomendacionDTO(), cliente);
 		cliente.setRecomendacion(rec);
-		
+
+		// Cargamos la lista de cuentas
+		for (int i = 0; i < clienteDTO.getListaCuentasDTO().size(); i++) {
+			Cuenta cuenta = CuentaDTO.convertToEntity(clienteDTO.getListaCuentasDTO().get(i));
+			cliente.getListaCuentas().add(cuenta);
+		}
+
 		return cliente;
 	}
 
-	//empty constructor
+	// empty constructor
 	public ClienteDTO() {
 		super();
 		this.recomendacionDTO = new RecomendacionDTO();
+		this.listaCuentasDTO= new ArrayList<CuentaDTO>();
 	}
-	
-	
+
 }
