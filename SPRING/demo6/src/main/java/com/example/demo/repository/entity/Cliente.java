e904e0f0eeec5af99e2ed6a6d2da38f963bdb0ac
Maca <mariacavallerm@gmail.com>
2024-12-14 12:51:38 +0100

    add listaDirecciones & empty constructor

--- SPRING/demo6/src/main/java/com/example/demo/repository/entity/Cliente.java
+++ SPRING/demo6/src/main/java/com/example/demo/repository/entity/Cliente.java
@@ -1,15 +1,19 @@
 package com.example.demo.repository.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -45,6 +49,19 @@ public class Cliente {
 	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL, mappedBy="cliente")
 	private List<Cuenta> listaCuentas;
 	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+	//tabla que mantiene la relacion N-N
+	@JoinTable (
+			//Nombre de la tabla
+			name = "clientesdirecciones",
+			//columna que almacena el id de cleinte en la tabla clientesdirecciones
+			joinColumns = @JoinColumn(name = "idcliente"),
+			//columna que almacena el id de la direccion en la tabla clientesdirecciones
+			inverseJoinColumns = @JoinColumn (name = "iddireccion")
+			)
+	@ToString.Exclude
+	private List<Direccion> listaDirecciones;
+		
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -60,6 +77,13 @@ public class Cliente {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	public Cliente() {
+		super();
+		this.recomendacion = new Recomendacion();
+		this.listaCuentas = new ArrayList<Cuenta>();
+		this.listaDirecciones = new ArrayList<Direccion>();
 	}
 	
 }
