693e1964573d886872e4b080b5ce38534ec73905
Maca <mariacavallerm@gmail.com>
2024-12-18 20:11:08 +0100

    change direccionDTO to ClienteDireccionDTO

--- SPRING/demo6/src/main/java/com/example/demo/service/DireccionService.java
+++ SPRING/demo6/src/main/java/com/example/demo/service/DireccionService.java
@@ -3,11 +3,13 @@ package com.example.demo.service;
 import java.util.List;
 
 import com.example.demo.model.dto.ClienteDTO;
+import com.example.demo.model.dto.ClienteDireccionDTO;
 import com.example.demo.model.dto.DireccionDTO;
 
 public interface DireccionService {
 
-	List<DireccionDTO> findAllByCliente(ClienteDTO clienteDTO);
+	//List<DireccionDTO> findAllByCliente(ClienteDTO clienteDTO);
+	List<ClienteDireccionDTO> findAllByCliente(ClienteDTO clienteDTO);
 	void save(DireccionDTO direccionDTO);
 
 }
